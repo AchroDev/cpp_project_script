67b66e0a740b9ee8d0b1ed76fe64cff370ea4737
AchroDev <achrodev@proton.me>
2024-09-10 22:03:37 -0500

    Added .gitignore from script

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,9 @@ int main(){
     std::cout << "Hello World" << std::endl;
     std::cin.get();
 }
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    std::cin.get();
+}
