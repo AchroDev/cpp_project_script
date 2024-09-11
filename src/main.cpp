17e2fdf2e59647314a9db5c631b7eb6be6b9d314
Achro <achrodev@proton.me>
2024-09-10 22:31:28 -0500

    Update main.cpp to cleanup extra appendages

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,3 @@ int main(){
     std::cout << "Hello World" << std::endl;
     std::cin.get();
 }
-#include <iostream>
-
-int main(){
-    std::cout << "Hello World" << std::endl;
-    std::cin.get();
-}
-#include <iostream>
-
-int main(){
-    std::cout << "Hello World" << std::endl;
-    std::cin.get();
-}
