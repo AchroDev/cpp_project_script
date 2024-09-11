592ec7fd48882653d6b45341afad0038366b1207
AchroDev <achrodev@proton.me>
2024-09-10 22:14:12 -0500

    Updated script to add basic information to the README

--- project_build.sh
+++ project_build.sh
@@ -60,6 +60,53 @@ echo "# Prerequisites
 *.out
 *.app" >> .gitignore
 touch README.md
+echo "<a name=\"readme-top\"></a>
+# README.md
+
+[![MIT License][license-shield]][license-url]
+
+<!-- PROJECT LOGO -->
+<br />
+<div align=\"center\">
+  <a href=\"https://github.com/AchroDev/REPO_NAME\">
+    <img src =\"resources/LOGO\" alt=\"Logo\" width=\"200\" height=\"200\">
+  </a>
+<h3 align=\"center\"> Project Name ~ AchroDev </h3>
+
+  <p align=\"center\">
+    Project Description.
+    <br />
+  </p>
+</div>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+<a href='https://ko-fi.com/R6R3WKVOY' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
+</a>
+
+
+#
+
+## Table of Contents
+
+> 1. No Table of Contents for this project.
+#
+
+## Instructions
+> No instructions for this project.
+
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+* [README template][readme-template]
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[license-shield]: https://img.shields.io/github/license/AchroDev/AchroDev.svg?style=for-the-badge
+[license-url]: https://github.com/AchroDev/echto/blob/main/LICENSE.txt
+[readme-template]: https://github.com/othneildrew/Best-README-Template
+[Acknowledgements]: https://github.com/AchroDev/echto?tab=readme-ov-file#acknowledgements" >> README.md
 cd src/
 touch main.cpp
 echo "#include <iostream>
--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,9 @@ int main(){
     std::cout << "Hello World" << std::endl;
     std::cin.get();
 }
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    std::cin.get();
+}
